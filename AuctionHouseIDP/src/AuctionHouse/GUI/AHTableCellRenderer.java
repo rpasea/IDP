c674c3d3428d0acefe42f61b61e557e56e569fe9
Iulius Curt <iulius.curt@gmail.com>
2013-03-25 15:27:47 +0200

    Table border

--- AuctionHouseIDP/src/AuctionHouse/GUI/AHTableCellRenderer.java
+++ AuctionHouseIDP/src/AuctionHouse/GUI/AHTableCellRenderer.java
@@ -3,6 +3,8 @@ package AuctionHouse.GUI;
 import java.awt.Color;
 import java.awt.Component;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.TableCellRenderer;
@@ -20,6 +22,7 @@ public class AHTableCellRenderer implements TableCellRenderer {
 		if (value.getClass().getName().equals("javax.swing.JTable")) {
 
 			((Component) value).setBackground(Color.white);
+			((JTable)value).setBorder(BorderFactory.createMatteBorder(0, 0, 0, 0, Color.BLACK));
 
 			return (Component) value;
 		} else {
@@ -28,6 +31,7 @@ public class AHTableCellRenderer implements TableCellRenderer {
 							hasFocus, row, column);
 
 			c.setBackground(Color.white);
+			((JLabel)c).setBorder(BorderFactory.createMatteBorder(1, 0, 1, 0, Color.BLACK));
 			return c;
 		}
 
